b502c65c756ac55652d88dc35a535ecba92fea25
kimiashz <shzkimia@gmail.com>
2020-06-08 17:18:10 +0430

    login component

--- login/src/App.vue
+++ login/src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <login-form/>
+    <nav-bar/>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import NavBar from './components/Nav.vue';
+import LoginForm from './components/Login.vue';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name:'login',
+  components:{
+    NavBar,
+    LoginForm
   }
 }
+</script>
+
+<style lang="sass">
+  @import './sass/main.sass';
+
+  #nav 
+    padding: $padding
+
+    a 
+      font-weight: bold
+      color: $gray
+      display: inline
+
+      &.router-link-exact-active 
+        color: $color 
 </style>
---
+++ login/src/components/Login.vue
@@ -0,0 +1,69 @@
+<template>
+    <form class="login" @submit.prevent="login">
+    <h1>KiMiA</h1>
+    <h6>Sign in</h6>
+    <input type="text" placeholder="username" required v-model="username"/>
+    <input type="password" placeholder="password" required v-model="password"/>
+    <button type="submit">Login</button>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'login',
+    data:function(){
+        return{
+            username:'',
+            password:''
+        }
+    },
+    methods:{
+        login: function(){
+            const { username, password } = this;
+            console.log('login',username, password);
+        }
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+    @import '@/sass/main.sass';
+    $width: 300px;
+    .login
+        margin: auto
+        h1, h6
+            margin: $margin 0
+        &, & > *
+            width: 100%
+            max-width: $width
+
+        *
+            display: block
+            box-sizing: border-box    
+            outline: none
+
+        h1
+            color: $color
+
+        button
+            background: $color
+            border: 0
+            padding: $padding
+            color: $white
+            &:hover
+                background: $color-light
+            &:focus
+                background: $color-dark
+
+        input
+            border: none
+            background: $white
+
+            &[type="text"], &[type="password"]
+                border-bottom: $border
+                padding: $padding
+                margin-bottom: $margin
+                &:focus
+                    border-bottom: 1px solid $color
+                    box-shadow: $box-shadow
+</style>
